64dc28731ae63b7df67bcaf66bdd8326fea3f1b5
cristian <cr.m23@hotmail.com>
2025-04-09 10:13:08 +0200

    operadores basicos y avanzados

---
+++ src/asincronia_prototipos_clases/OperadoresAvanzados.js
@@ -0,0 +1,31 @@
+//Operadores de suma, resta, multiplicacion, division
+
+let number = 5;
+let number2 = 10;
+
+let sum = number + number2;
+let resta = number - number2;
+let producto = number * number2;
+let division = number / number2;
+
+console.log("Suma: " + sum +
+"\nResta: " + resta +
+"\nMultiplicacion: " + producto +
+"\nDivision: " + division);
+
+//operadores de modulo, incremento, decremento, exponente, negativo
+let number3 = 5;
+
+let mod = number3 % 2;
+console.log("Modulo: " + mod);
+let incremento = number3++;
+console.log("Incremento: " + incremento);
+let decremento = number3--;
+console.log("Decremento: " + decremento);
+let exponente = number3 ** 3;
+console.log("Exponente: " + exponente);
+let negativo = -number3;
+console.log("Negativo: " + negativo);
+
+
+
